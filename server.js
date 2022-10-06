09b1d89643d1d6b731f36156fcec0ba392bb5607
Emma Maksimava <cielvibritania@gmail.com>
2022-10-06 12:11:39 +0200

    feat: add server part

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use('/js', express.static(path.resolve(__dirname, 'frontend', 'js')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'index.html'));
+});
+
+app.listen(process.env.PORT || 5060, () => console.log('Server running...'));
