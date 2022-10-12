f1ab177e593db518c9f86736eebde5a4177518de
Emma Maksimava <cielvibritania@gmail.com>
2022-10-12 18:37:34 +0200

    feat: add settings class

---
+++ frontend/js/views/Settings.js
@@ -0,0 +1,26 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView {
+  constructor() {
+    super();
+    this.setTitle('Posts');
+
+
+  }
+
+  async getHtml() {
+    return `
+    <div class="settings">
+      <h2 class="set-title">Settings</h2>
+      <p class="text-content">Manage your privacy and configuration</p>
+      <ul class="list">
+        <li class="list-item"><a href="#" class="list-link">Password and Safety</a></li>
+        <li class="list-item"><a href="#" class="list-link">Color Theme</a></li>
+        <li class="list-item"><a href="#" class="list-link">Display</a></li>
+        <li class="list-item"><a href="#" class="list-link">Notifications</a></li>
+      </ul>
+
+    </div>
+    `;
+  }
+}
